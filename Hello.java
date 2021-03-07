1b380c249d8986f1a08508582d8043c6fac2d28e
‘mldn’ <mldnqa@163.com>
2021-03-07 22:51:26 +0800

    modify file

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("学习使我快乐！");
+		System.out.println("学习使我快乐22222！");
 		Date date = new Date();
 	}
 }
