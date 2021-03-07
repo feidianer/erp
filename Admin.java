9fba8fcbc8b5d53dec232bb54025b09e791bb9ef
‘mldn’ <mldnqa@163.com>
2021-03-07 22:31:32 +0800

    delete all file

--- Admin.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-public class Admin{
-	public static void main(String args[]){
-		private List<Emp> emps;
-	}
-}
--- Hello.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Hello{
-	public static void main(String args[]){
-		System.out.println("世界你好！");
-		Admin admin = new Admin();
-		Emp emp = new Emp();
-	}
-}
