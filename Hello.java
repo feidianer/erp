16609d8d4afbe889fdceae214d43919468a62e76
‘mldn’ <mldnqa@163.com>
2021-03-07 22:20:37 +0800

    change Hello.java in master branch

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class Hello{
 	public static void main(String args[]){
-		System.out.println("学习使我快乐！");
-		Date date = new Date();
+		System.out.println("世界你好！");
+		Admin admin = new Admin();
+		Emp emp = new Emp();
 	}
 }
