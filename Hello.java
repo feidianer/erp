ed90a4dea87ad8b92c2a91df4d1017ea1ea3391b
‘mldn’ <mldnqa@163.com>
2021-03-07 22:39:58 +0800

    add files

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+	public static void main(String args[]){
+		//System.out.println("Hello Git!");
+		System.out.println("Hello Git11111!");
+		System.out.println("Hello Git22222!");
+		System.out.println("世界你好！");
+		System.out.println(new java.util.Date());
+	}
+}
