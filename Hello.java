f0b94e7e5f38cbb154e3b06c6e2a6837aded7800
‘mldn’ <mldnqa@163.com>
2021-03-14 00:06:40 +0800

    Test Merge

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("世界你好！");
+		System.out.println("世界你好2！");
 		Admin admin = new Admin();
 		Emp emp = new Emp();
 		System.out.println("学习使我快乐！");
